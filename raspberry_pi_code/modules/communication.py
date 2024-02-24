409486bdeef170a5e76919b5e28c84f9ea57c54d
PaulAllan209 <dpaulallan15@gmail.com>
2024-02-24 08:31:20 +0800

    setting the pseudo code for the arduino comms

--- raspberry_pi_code/modules/communication.py
+++ raspberry_pi_code/modules/communication.py
@@ -1,16 +1,42 @@
 import serial
 from time import sleep
 
-arduino_mega_ser = serial.Serial('COM5', 9600, xonxoff=True)
 
 
-def tester():
-    return b"wowmagic"
+class ArduinoMegaCommunication:
+    def __init__(self, port='/dev/ttyS0', baudrate=9600, xonxoff=True) -> None:
+        self.serial_port = serial.Serial(port, baudrate, xonxoff=xonxoff)
+        
+        
+    def send_data(self, data: dict):
+        # Data needed to be sent:
+        # Number of pulses for the 4 peristaltic pumps controlled by L298N
+        # Water pump speed controlled by L298N
+        # Peltier cooler state (turn on and hot or cold?)
+        # Linear actuators state
+        # Grow light 1, 2, 3 state
+        # Air pump state
+        # Exhaust fan 1 state
+        # Exhaust fan 2 state
+        # Solenoid valve state
+        # Stepper motor X, Y, Z1, Z2 locations 
+        
+        base_peris_pump = data["base_peris_pump"]
+        acid_peris_pump = data["acid_peris_pump"]
+        nutriA_peris_pump = data["nutriA_peris_pump"]
+        nutriB_peris_pump = data["nutriB_peris_pump"]
+        
+        
+        
+        
     
+    def receive_data(self, data):
+        pass
     
+    def open(self):
+        self.serial_port.open()
     
-while True:
-    arduino_mega_ser.write(tester())
-    sleep(1)
-    
+    def close(self):
+        self.serial_port.close()
+        
     
