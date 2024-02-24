ed7c3a456a4a2c5483a0706b735f873f3356a16a
PaulAllan209 <dpaulallan15@gmail.com>
2024-02-24 14:33:00 +0800

    setting up the send data method for arduino comms

--- raspberry_pi_code/main.py
+++ raspberry_pi_code/main.py
@@ -1,2 +1,9 @@
 from modules import communication
 
+ArduinoComms = communication.ArduinoMegaCommunication()
+
+data = {
+    "base_peris_pump":2,
+    "acid_peris_pump":2,
+    
+}
--- raspberry_pi_code/modules/communication.py
+++ raspberry_pi_code/modules/communication.py
@@ -6,26 +6,64 @@ from time import sleep
 class ArduinoMegaCommunication:
     def __init__(self, port='/dev/ttyS0', baudrate=9600, xonxoff=True) -> None:
         self.serial_port = serial.Serial(port, baudrate, xonxoff=xonxoff)
+        self.serial_port.open()
         
         
-    def send_data(self, data: dict):
+    def send_data(self, arduino_data: dict):
         # Data needed to be sent:
         # Number of pulses for the 4 peristaltic pumps controlled by L298N
         # Water pump speed controlled by L298N
         # Peltier cooler state (turn on and hot or cold?)
         # Linear actuators state
-        # Grow light 1, 2, 3 state
+        # Grow light 1, 2 state
         # Air pump state
         # Exhaust fan 1 state
         # Exhaust fan 2 state
         # Solenoid valve state
         # Stepper motor X, Y, Z1, Z2 locations 
         
-        base_peris_pump = data["base_peris_pump"]
-        acid_peris_pump = data["acid_peris_pump"]
-        nutriA_peris_pump = data["nutriA_peris_pump"]
-        nutriB_peris_pump = data["nutriB_peris_pump"]
         
+        # Peristaltic pumps
+        base_peris_pump = arduino_data["base_peris_pump"]
+        acid_peris_pump = arduino_data["acid_peris_pump"]
+        nutriA_peris_pump = arduino_data["nutriA_peris_pump"]
+        nutriB_peris_pump = arduino_data["nutriB_peris_pump"]
+        
+        # Water pump
+        water_pump_speed = arduino_data["water_pump_speed"]
+        
+        # Peltier cooler
+        peltier_state = arduino_data["peltier_state"] # on or off?
+        peltier_mode = arduino_data["peltier_mode"] # hot or cold?
+        
+        # Linear actuators state
+        linear_act = arduino_data["linear_act"]
+        
+        
+        # Grow light 1 and 2 state
+        grow_light_1 = arduino_data["grow_light_1"]
+        grow_light_2 = arduino_data["grow_light_2"]
+        
+        # Air pump state
+        air_pump = arduino_data["air_pump"]
+        
+        # Exhaust fan 1 and 2 state
+        exhaust_fan_1 = arduino_data["exhaust_fan_1"]
+        exhaust_fan_2 = arduino_data["exhaust_fan_2"]
+        
+        # Solenoid valve state
+        solenoid = arduino_data["solenoid"]
+        
+        # Stepper motor positions
+        step_x = arduino_data["step_x"]
+        step_y = arduino_data["step_y"]
+        step_z1 = arduino_data["step_z1"]
+        step_z2 = arduino_data["step_z2"]
+        
+        data_str = f"{base_peris_pump},{acid_peris_pump},{nutriA_peris_pump},{nutriB_peris_pump},{water_pump_speed},{peltier_state},{peltier_mode},{linear_act},
+        {grow_light_1},{grow_light_2},{air_pump},{exhaust_fan_1},{exhaust_fan_2},{solenoid},{step_x},{step_y},{step_z1},{step_z2}"
+        
+        self.serial_port.write(data_str.encode('utf-8'))
         
         
         
