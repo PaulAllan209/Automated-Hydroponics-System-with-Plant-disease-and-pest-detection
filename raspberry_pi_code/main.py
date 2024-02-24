a30dff5fa6a143fe96f0255fc68aa96dba70675a
PaulAllan209 <dpaulallan15@gmail.com>
2024-02-24 14:37:58 +0800

    getting ready for testing the arduino comms

--- raspberry_pi_code/main.py
+++ raspberry_pi_code/main.py
@@ -5,5 +5,30 @@ ArduinoComms = communication.ArduinoMegaCommunication()
 data = {
     "base_peris_pump":2,
     "acid_peris_pump":2,
+    "nutriA_peris_pump":2,
+    "nutriB_peris_pump":2,
     
+    "water_pump_speed":0.5,
+    
+    "peltier_state":True,
+    "peltier_mode":"Cold",
+    
+    "linear_act":True,
+    
+    "grow_light_1":True,
+    "grow_light_2":True,
+    
+    "air_pump":True,
+    
+    "exhaust_fan_1":True,
+    "exhaust_fan_2":True,
+    
+    "solenoid":False,
+    
+    "step_x":200,
+    "step_y":200,
+    "step_z1":200,
+    "step_z2":200
 }
+
+ArduinoComms.send_data(data)
