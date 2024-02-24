0dae14e5fb01b72d2fdc65d5ac69f17c5ddde8e6
PaulAllan209 <dpaulallan15@gmail.com>
2024-02-24 17:50:34 +0800

    fixed again the f strings

--- raspberry_pi_code/modules/communication.py
+++ raspberry_pi_code/modules/communication.py
@@ -60,24 +60,26 @@ class ArduinoMegaCommunication:
         step_z1 = arduino_data["step_z1"]
         step_z2 = arduino_data["step_z2"]
         
-        data_str = (f'base_peris_pump,{base_peris_pump},
-                    acid_peris_pump,{acid_peris_pump},
-                    nutriA_peris_pump,{nutriA_peris_pump},
-                    nutriB_peris_pump,{nutriB_peris_pump},
-                    water_pump_speed,{water_pump_speed},
-                    peltier_state,{peltier_state},
-                    peltier_mode,{peltier_mode},
-                    linear_act,{linear_act},
-                    grow_light_1,{grow_light_1},
-                    grow_light_2,{grow_light_2},
-                    air_pump,{air_pump},
-                    exhaust_fan_1,{exhaust_fan_1},
-                    exhaust_fan_2,{exhaust_fan_2},
-                    solenoid,{solenoid},
-                    step_x,{step_x},
-                    step_y,{step_y},
-                    step_z1,{step_z1},
-                    step_z2,{step_z2}')
+        data_str = (
+    f'base_peris_pump,{base_peris_pump},'
+    f'acid_peris_pump,{acid_peris_pump},'
+    f'nutriA_peris_pump,{nutriA_peris_pump},'
+    f'nutriB_peris_pump,{nutriB_peris_pump},'
+    f'water_pump_speed,{water_pump_speed},'
+    f'peltier_state,{peltier_state},'
+    f'peltier_mode,{peltier_mode},'
+    f'linear_act,{linear_act},'
+    f'grow_light_1,{grow_light_1},'
+    f'grow_light_2,{grow_light_2},'
+    f'air_pump,{air_pump},'
+    f'exhaust_fan_1,{exhaust_fan_1},'
+    f'exhaust_fan_2,{exhaust_fan_2},'
+    f'solenoid,{solenoid},'
+    f'step_x,{step_x},'
+    f'step_y,{step_y},'
+    f'step_z1,{step_z1},'
+    f'step_z2,{step_z2}'
+)
         
         self.serial_port.write(data_str.encode('utf-8'))
         
