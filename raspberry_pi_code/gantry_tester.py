48402246c0614e49dd3f8f1b62b599803e3903da
PaulAllan209 <dpaulallan15@gmail.com>
2024-05-25 13:35:55 +0800

    added code for testing the gantry in raspberry pi

--- raspberry_pi_code/gantry_tester.py
+++ raspberry_pi_code/gantry_tester.py
@@ -1,30 +1,34 @@
 import serial
+import time
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, rtscts=True)
 
+# 114 - capture plants
+# 115 - next plant state
+# 116 - return home position
+
+# delay for each plant interval
+del_plant = 10
 
 # Sends command to mega to start the capture of plants
 ser.write(b"<114,1>")
 
-# receives message if the gantry is in home position
-while True:
-    bytes_waiting = ser.in_waiting()
-    if bytes_waiting > 0:
-        data_str = ser.readline().decode('ascii').strip()
-        
-        if data_str == 'at_home':
-            ser.write(b"<115, 1>")
-        
-        if data_str == "plant_2":
-            ser.write(b"<115, 2>")
-        
-        
+# Sends command to return to home position
+ser.write(b"<116,1>") # dont add spaces in commands
+# Wait 40s
+time.sleep(40)
+
+# Sends command to turn off home position
+ser.write(b"<116,0>")
+
+# Sends command to go to plant 1
+ser.write(b"<115, 1>")
+# Wait 35s
+time.sleep(35)
+
+# Sends command to go to plant 2
+ser.write(b"<115, 2>")
+# Wait 10s
+time.sleep(10)
 
 
-
-# Go to plant 1
-# Wait until it is on plant 1
-
-# If it is on plant 1 go to plant 2
-
-#Repeat until plant 10
